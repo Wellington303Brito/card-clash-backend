4aa0a6f846407ea51fd7c0a55f53616dcb1b5aa4
Seu Nome <wbritos640@gmail.com>
2026-03-24 11:43:34 -0300

    corrige conexao mysql com poll

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: Number(process.env.DB_PORT) || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 console.log("DB_HOST:", process.env.DB_HOST);
@@ -13,11 +16,12 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_PORT:", process.env.DB_PORT);
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("Erro ao conectar no MySQL:", err);
   } else {
     console.log("Conectado ao MySQL!");
+    connection.release();
   }
 });
 
